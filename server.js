7d5f4a42a933ef1a2f38322c84490b5c7d2e7b09
JosafatPadilla <j.padilla.chi@gmail.com>
2018-03-06 15:24:29 -0600

    - change adapter

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
 var gps = require("gps-tracking");
+ 
+var options = {
+    'debug'                 : true,
+    'port'                  : process.env.PORT || 5000,
+    'device_adapter'        : "TK103_2"
+}
+ 
+var server = gps.server(options,function(device,connection){
+ 
+    device.on("login_request",function(device_id,msg_parts){
+ 
+        // Some devices sends a login request before transmitting their position 
+        // Do some stuff before authenticate the device... 
+        console.log(device_id);
+        // Accept the login request. You can set false to reject the device. 
+        this.login_authorized(true);
+ 
+    });
+ 
+ 
+    //PING -> When the gps sends their position 
+    device.on("ping",function(data){
+        console.log(data);
+        //After the ping is received, but before the data is saved 
+        //console.log(data); 
+        return data;
+ 
+    });
+ 
+});
+/*var gps = require("gps-tracking");
 
 var options = {
     'debug'                 : false, //We don't want to debug info automatically. We are going to log everything manually so you can check what happens everywhere
@@ -47,4 +78,4 @@ var server = gps.server(options,function(device,connection){
         console.log(data.toString()); 
     })
 
-});
+});*/
